0f5ac9f70d307ff6e3378626f4ee2276aa8fbf2c
jismimathew <jismimathew18@gmail.com>
2022-11-06 22:00:56 -0500

    please commit

---
+++ Demo-main/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("com.pragra.Hello world!");
+        System.out.println("hi");
+    }
+}
---
+++ Demo-main/src/com/pragra/Add.java
@@ -0,0 +1,10 @@
+package com.pragra;
+
+public class Add {
+    public static void main(String[] args) {
+        System.out.println("sum of two number");
+        int c= 6+10;
+        System.out.println(c);
+        System.out.println("hi");
+    }
+}
